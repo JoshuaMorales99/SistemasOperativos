4937792dc4eb8b3613298fc392d134fc0b38a274
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2023-08-26 22:38:11 +0000

    Creacion del TP inicial y TP 0

---
+++ TP Inicial/HolaMundo/src/HolaMundo.c
@@ -0,0 +1,12 @@
+/* ============================================================================
+ Name        : HolaMundo.c
+ Description : Hello World in C,
+ ============================================================================ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!"); // prints !!!Hello World!!!
+	return EXIT_SUCCESS;
+}
